dc11fd691a753277cb6ab9640ad7610fa14a9862
xiemenger <tbaby.menger@gmail.com>
2018-05-20 20:24:39 -0400

    use set whose size is limite to k

---
+++ Algorithms/219ContainsDuplicateII.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++){
+            if (!set.add(nums[i])){
+                return true;
+            }
+            if (set.size() > k){
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
