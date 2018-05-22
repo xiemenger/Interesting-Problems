0f169d2c11c2f0dba9633745cde2b261601fa6cf
xiemenger <tbaby.menger@gmail.com>
2018-05-22 16:08:55 -0400

    use two stacks

---
+++ Algorithms/232ImplementQueueUsingStacks.java
@@ -0,0 +1,54 @@
+class MyQueue {
+
+    /** Initialize your data structure here. */
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (stack2.isEmpty()){
+            this.Stack1PullIntoStack2();
+        }
+        return stack2.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if (stack2.isEmpty()){
+            this.Stack1PullIntoStack2();
+        }
+        return stack2.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        if (stack2.isEmpty()){
+            this.Stack1PullIntoStack2();
+        }
+        return stack2.isEmpty();
+    }
+    
+    public void Stack1PullIntoStack2(){
+        while (!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
