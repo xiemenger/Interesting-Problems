6fea00c1554d7eeac606ab164305008cef58b744
xiemenger <tbaby.menger@gmail.com>
2018-05-22 12:34:02 -0400

    Use one queue, for push, push the new element. and reverse the entire queue

---
+++ Algorithms/225ImplementStackUsingQueues.java
@@ -0,0 +1,42 @@
+class MyStack {
+
+    /** Initialize your data structure here. */
+    Queue<Integer> queue;
+    
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue.add(x);
+        int size = queue.size() - 1;
+        for (int i = 0; i < size; i++){
+            queue.add(queue.poll());
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.poll();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return queue.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
--- Algorithms/226InvertBinaryTree.java
+++ Algorithms/226InvertBinaryTree.java
@@ -8,7 +8,45 @@
  * }
  */
 class Solution {
-    public TreeNode invertTree(TreeNode root) {
+    public TreeNode invertTree(TreeNode root){
+        if (root == null){
+            return root;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            if (node.left != null){
+                queue.offer(node.left);
+            }
+            if (node.right != null){
+                queue.offer(node.right);
+            }
+            TreeNode tmp = node.left;
+            node.left = node.right;
+            node.right = tmp;
+        }
+        return root;
+    }
+    
+    
+    public TreeNode invertTree1(TreeNode root){
+        invertTreeHelper(root);
+        return root;
+    }
+    
+    public void invertTreeHelper(TreeNode root){
+        if (root == null){
+            return;
+        }
+        invertTreeHelper(root.left);
+        invertTreeHelper(root.right);
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+    }
+    
+    public TreeNode invertTree2(TreeNode root) {
         if (root == null){
             return root;
         }
