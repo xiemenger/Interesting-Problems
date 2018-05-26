a696641381b9afc3670b744d09adb3debd77b560
xiemenger <tbaby.menger@gmail.com>
2018-05-26 15:19:20 -0400

    if cur value is larger than the top value of the stack, pop out the smaller ones in the stack

---
+++ Algorithms/255VerifyPreorderSequenceInBinarySearchTree.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean verifyPreorder(int[] preorder) {
+        int low = Integer.MIN_VALUE;
+        Stack<Integer> stack = new Stack<>();
+        for (int p: preorder){
+            if (p < low){
+                return false;
+            }
+            while (!stack.isEmpty() && p > stack.peek()){
+                low = stack.pop();
+            }
+            stack.push(p);
+        }
+        return true;
+    }
+}
