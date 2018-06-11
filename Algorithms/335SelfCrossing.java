a03a23255fbd253b4314e6412a0f287807424832
xiemenger <tbaby.menger@gmail.com>
2018-06-10 23:54:43 -0400

    three cases

---
+++ Algorithms/335SelfCrossing.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSelfCrossing(int[] x) {
+        for (int i = 3; i < x.length; i++){
+            if (x[i - 2] <= x[i] && x[i-3] >= x[i-1]){ return true; }
+            if (i >= 4 && x[i-1] == x[i - 3] && x[i]+x[i-4] >= x[i-2]){ return true; }
+            if (i >= 5){
+                if (x[i-2] - x[i-4] >= 0 && x[i] >= x[i-2] - x[i-4] && x[i-1] >= x[i-3] - x[i-5] && x[i-1] <= x[i-3]) {
+                    return true;
+                }    
+            } 
+        }
+        return false;
+    }
+}
