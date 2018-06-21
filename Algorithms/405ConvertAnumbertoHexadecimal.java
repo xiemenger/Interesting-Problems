a3e2c70f940d648e27318586a8cfd51829be87c8
xiemenger <tbaby.menger@gmail.com>
2018-06-21 13:40:05 -0400

    bit operation

---
+++ Algorithms/405ConvertAnumbertoHexadecimal.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String toHex(int num) {
+        if (num == 0){
+            return "0";
+        }
+        char[] map = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        String res = "";
+        
+        while (num != 0){
+            res = (map[num & 15]) + res;
+            num >>>= 4;
+        }
+        return res;
+    }
+}
