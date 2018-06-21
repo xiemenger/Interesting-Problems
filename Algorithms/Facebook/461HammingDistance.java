6b1204f153f82ec48fcf30e1d9e205d6f3660060
xiemenger <tbaby.menger@gmail.com>
2018-06-21 15:28:00 -0400

    bit operation

---
+++ Algorithms/Facebook/461HammingDistance.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        for (int i = 0; i < 32; i++){
+            if ( (x & 1) != (y & 1)){
+                count++;
+            }
+            x >>>= 1;
+            y >>>= 1;
+        }
+        return count;
+    }
+}
