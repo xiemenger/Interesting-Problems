649d4cace5818a5e59c7b6015553eb7634824867
xiemenger <tbaby.menger@gmail.com>
2018-07-09 20:30:45 -0700

    dp

---
+++ Algorithms/Google/474OnesAndZeroes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        for (String s : strs){
+            int zeros = 0;
+            int ones = 0;
+            for (char c : s.toCharArray()){
+                if (c == '0'){
+                    zeros++;
+                }
+            }
+            ones = s.length() - zeros;
+            
+            for (int i = m; i >= zeros; i--){
+                for (int j = n; j >= ones; j--){
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
