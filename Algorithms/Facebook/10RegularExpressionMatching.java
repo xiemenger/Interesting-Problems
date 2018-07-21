f5a72c3877fd6273a908b170bd58e050e7dfdac4
xiemenger <tbaby.menger@gmail.com>
2018-07-21 12:55:16 -0700

    match once, match multiple times, match none

---
+++ Algorithms/Facebook/10RegularExpressionMatching.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int slen = s.length();
+        int plen = p.length();
+        boolean[][] canMatch = new boolean[slen + 1][plen + 1];
+        canMatch[0][0] = true;
+        for (int i = 1; i <= plen; i++){
+            if (p.charAt(i - 1) == '*' && canMatch[0][i - 2]){
+                canMatch[0][i] = true;
+            }
+        }
+                
+        for (int i = 1; i <= slen; i++){
+            char scur = s.charAt(i - 1);
+            for (int j = 1; j <= plen; j++){
+                char pcur = p.charAt(j  - 1);
+                if (scur == pcur || pcur == '.'){
+                    canMatch[i][j] = canMatch[i- 1][j - 1];
+                } else if (pcur == '*'){
+                    if (scur != p.charAt(j - 2) && p.charAt(j - 2) != '.'){
+                        canMatch[i][j] = canMatch[i][j - 2];
+                    } else {
+                        canMatch[i][j] = canMatch[i][j - 2] || canMatch[i][j - 1] || canMatch[i - 1][j];
+                    }
+                }
+            }
+        }
+        return canMatch[slen][plen];
+    }
+}
