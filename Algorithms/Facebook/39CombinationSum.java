be7167a8ec43bf23d4e6bfb1a66873d5e2fd0022
xiemenger <tbaby.menger@gmail.com>
2018-07-22 14:29:09 -0700

    backtrack

---
+++ Algorithms/Facebook/39CombinationSum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (candidates == null || candidates.length == 0){
+            return res;
+        }
+        Arrays.sort(candidates);
+        combinate(candidates, 0, target, new ArrayList<>(), res);
+        return res;
+    }
+    
+    private void combinate(int[] cand, int pos, int target, List<Integer> list, List<List<Integer>> res){
+        if (target == 0){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        if (pos == cand.length){
+           return; 
+        }
+        for (int i = pos; i < cand.length; i++){
+            if (target >= cand[i]){
+                list.add(cand[i]);
+                combinate(cand, i, target - cand[i], list, res);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
