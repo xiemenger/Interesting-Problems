cacfe29ffc8612d17ee623864717c0707c250213
xiemenger <tbaby.menger@gmail.com>
2018-08-01 21:17:33 -0700

    simular to next permutation

---
+++ Algorithms/Facebook/PreviousNum-ChangeFromNextPermutation.java
@@ -0,0 +1,19 @@
+	private static void previousNum(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return;
+		}
+		int n = nums.length;
+		int fIidx = n - 2;
+		while (fIidx >= 0 && nums[fIidx ] <= nums[fIidx + 1]) {
+			fIidx--;
+		}
+		if (fIidx >= 0) {
+			int flidx = n - 2;
+			while (flidx >= 0 && nums[flidx] >= nums[flidx + 1]) {
+				flidx--;
+			}
+			swap(nums, fIidx, flidx);
+		}
+		reverse(nums, fIidx + 1, n - 1);
+		printNum(nums);
+	} 
