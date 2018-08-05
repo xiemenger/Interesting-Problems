aa40904d5af34b964714031fe49f4e0625fbf9fe
xiemenger <tbaby.menger@gmail.com>
2018-08-04 21:12:40 -0700

    linkedlist

---
+++ Algorithms/Facebook/21MergeTwoSortedLists.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null){return l2; }
+        if (l2 == null){return l1; }
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (l1 != null && l2 != null){
+            if (l1.val <= l2.val){
+                tail.next = l1;
+                l1 = l1.next;
+                
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        while (l1 != null){
+            tail.next = l1;
+            l1 = l1.next;
+            tail = tail.next;
+        }
+        while (l2 != null){
+            tail.next = l2;
+            l2 = l2.next;
+            tail = tail.next;
+        }
+        return dummy.next;
+    }
+}
