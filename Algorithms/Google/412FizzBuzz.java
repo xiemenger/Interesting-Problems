d5213669053773b79c916ea175c67de24162c4b7
xiemenger <tbaby.menger@gmail.com>
2018-08-28 23:22:47 -0400

    not use mod

---
+++ Algorithms/Google/412FizzBuzz.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+        for (int i = 1, fizz = 0, buzz = 0; i <= n; i++){
+            fizz++;
+            buzz++;
+            if (fizz == 3 && buzz == 5){
+                res.add("FizzBuzz");
+                fizz = 0;
+                buzz = 0;
+            } else if (fizz == 3){
+                res.add("Fizz");
+                fizz = 0;
+            } else if (buzz == 5){
+                res.add("Buzz");
+                buzz = 0;
+            } else {
+                res.add(i+"");
+            }
+        }
+        return res;
+    }
+}
