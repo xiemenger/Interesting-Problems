8b65889f7286b17d77fe5a4fd07a28d6d4ebba79
xiemenger <tbaby.menger@gmail.com>
2018-08-31 00:00:01 -0400

    check overflow

---
+++ Algorithms/Airbnb/7.ReverseInteger.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        int res = 0;
+        while (x != 0){
+            int last = x % 10;
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && last > 7)){
+                return 0;
+            }
+            if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && last < -8)){
+                return 0;
+            }
+            res = res * 10 + last;
+            x /= 10;
+        }
+        return res;
+    }
+}
